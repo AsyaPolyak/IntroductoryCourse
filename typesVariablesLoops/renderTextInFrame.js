d9b82f32e079f5629d1d0fe8afbc1b25e0bba1f3
AsyaPolyak <asyapolyachka@gmail.com>
2022-07-15 15:05:10 +0300

    add for and if

--- typesVariablesLoops/renderTextInFrame.js
+++ typesVariablesLoops/renderTextInFrame.js
@@ -1,5 +1,24 @@
 function renderTextInFrame(text) {
-    let result = '';
-    result += text; // заміни власним кодом
+    let temp ="";
+    let result = '*';
+    t = (text.length - 1) + 8;//довжина строчки 
+  
+    for(let j = 0; j <= t ;j +=1) {
+      temp = "".concat(temp,'*');
+    }
+    console.log(temp);
+    if(text.length) { 
+     res = result +"   " + text + "   "+result; 
+    }
+    //console.log(temp.repeat(1));
+    //result = text; // заміни власним кодом
+    //console.log(temp.repeat());
+    res = temp.repeat(1);
+    console.log(temp.repeat(1));
+    //result= temp + res + temp_repeat;
     return result;
-}
+    
+  
+  } 
+  console.log(renderTextInFrame('Hello World'));
+  
